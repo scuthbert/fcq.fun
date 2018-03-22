fb6ad167bf81fe579619a0d326b968a2198dd8fd
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2018-03-22 10:23:16 -0600

    Fixed linter errors

--- fcq.fun/src/app/local-data-cache.ts
+++ fcq.fun/src/app/local-data-cache.ts
@@ -2,11 +2,11 @@ import { DataStore } from "./data-store";
 import { Plottable } from "./plottable";
 
 export class LocalDataCache implements DataStore {
-  private localCache: {[key: string]: Plottable}
+  private localCache: {[key: string]: Plottable};
 
   public getPlottable(name: string): Plottable {
     let result: Plottable = this.localCache[name];
-    if(result == null) {
+    if (result == null) {
       // Call HTTPRequestor
     }
     return result;
