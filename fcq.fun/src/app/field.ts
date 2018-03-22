d2b796c849fda10a41782b62325f69f130885b28
Connor Hudson <cohu3291@colorado.edu>
2018-03-21 20:43:34 -0600

    Implement field class

--- fcq.fun/src/app/field.ts
+++ fcq.fun/src/app/field.ts
@@ -1,2 +1,28 @@
 export class Field {
+    private values: number[];
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    public getValues(): number[] {
+        return this.values;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public setValues(values: number[]): void {
+        this.values = values;
+    }
+
+    public appendValue(newValue: number): void {
+        this.values.push(newValue);
+    }
+
+    public setName(newName: string): void {
+        this.name = newName;
+    }
 }
