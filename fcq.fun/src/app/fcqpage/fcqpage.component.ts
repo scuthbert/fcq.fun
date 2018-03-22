0d5fd42702c1bda47728d444b20dbc7d9413fcb0
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2018-03-21 21:21:27 -0600

    Added DataStore to FCQPage

--- fcq.fun/src/app/fcqpage/fcqpage.component.ts
+++ fcq.fun/src/app/fcqpage/fcqpage.component.ts
@@ -2,21 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 import { Lecturer } from '../lecturer';
 import { Plottable } from '../plottable';
+import { DataStore } from '../data-store';
+import { LocalDataCache } from '../local-data-cache';
 
 @Component({
   selector: 'app-fcqpage',
   templateUrl: './fcqpage.component.html',
   styleUrls: ['./fcqpage.component.css']
 })
+
 export class FCQPageComponent implements OnInit {
   title: string = "DEFAULT";
   currentQuery: Plottable;
+  dataInterface: DataStore;
 
   public search(name: string): void {
     //TODO: Search for String
   }
 
-  constructor() {}
+  constructor() {
+    dataInterface = new LocalDataCache();
+  }
 
   ngOnInit() {}
 
