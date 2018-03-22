73d6a619f60f08b6b333259eb226e745030ba397
Connor Hudson <cohu3291@colorado.edu>
2018-03-21 21:21:39 -0600

    Implement Lecturer class

--- fcq.fun/src/app/lecturer.ts
+++ fcq.fun/src/app/lecturer.ts
@@ -1,9 +1,33 @@
-import { Plottable } from './plottable';
+import { Plottable } from "./plottable";
+import { Field } from "./field";
 
 export class Lecturer implements Plottable {
-  public Name: string;
+  private name: string;
+  private fieldList: Field[];
+  private valid: boolean;
 
-  constructor() {
-    this.Name = "test";
+  public constructor(name: string) {
+    this.name = name;
+  }
+
+  public getLecturerName(): string {
+    return this.name;
+  }
+
+  public getFieldData(fieldName: string) {
+    let foundField = this.fieldList.find((field: Field) => field.getName() === fieldName);
+    if (foundField) {
+      return foundField.getValues();
+    } else {
+      throw new Error("No field was found for the provided name");
+    }
+  }
+
+  public getFieldList() {
+    return this.fieldList;
+  }
+
+  public getValid() {
+    return this.valid;
   }
 }
