f34f84cf05dac06ba31dfe8daef458da0bdfc142
Connor Hudson <cohu3291@colorado.edu>
2018-04-11 17:25:25 -0600

    Initial class setup for ChartDisplay

--- fcq.fun/src/app/chart-display/chart-display.component.ts
+++ fcq.fun/src/app/chart-display/chart-display.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { Plottable } from "../plottable";
 
 @Component({
   selector: "app-chart-display",
@@ -6,8 +7,17 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./chart-display.component.css"]
 })
 export class ChartDisplayComponent implements OnInit {
-
+  @Input() private currentFields: string[];
+  @Input() private currentResult: Plottable;
   constructor() { }
+
+  public updateFields(fields: string[]): void {
+    if (!fields.every(field => field in this.currentResult.getFieldList())) {
+      throw new Error("Some fields do not exist in result");
+    }
+
+    this.currentFields = fields;
+  }
 
   ngOnInit() {
   }
