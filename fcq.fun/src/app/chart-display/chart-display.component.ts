eef90ea7a93e534e0ec2c9a481ca819231989903
Connor Hudson <cohu3291@colorado.edu>
2018-04-11 18:20:45 -0600

    Initial implementation of display() and some angular lifecycle hooks

--- fcq.fun/src/app/chart-display/chart-display.component.ts
+++ fcq.fun/src/app/chart-display/chart-display.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { Plottable } from "../plottable";
+import * as Plotly from "plotly.js";
 
 @Component({
   selector: "app-chart-display",
   templateUrl: "./chart-display.component.html",
   styleUrls: ["./chart-display.component.css"]
 })
-export class ChartDisplayComponent implements OnInit {
+export class ChartDisplayComponent implements OnChanges {
   @Input() private currentFields: string[];
   @Input() private currentResult: Plottable;
   constructor() { }
@@ -19,7 +20,24 @@ export class ChartDisplayComponent implements OnInit {
     this.currentFields = fields;
   }
 
-  ngOnInit() {
+  public display(data: Plottable): void {
+    let traces: {}[];
+    this.currentFields.forEach(field => {
+      traces.push({
+        name: field,
+        mode: "lines+markers",
+        type: "scatter",
+        line: {shape: "spline"},
+        y: data.getFieldData(field)
+      });
+    });
+
+    Plotly.react("data-chart", traces);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // this.display(changes["CurrentResult"].currentValue);
+    this.display(this.currentResult);
   }
 
 }
