0bb6581210d517ea043af1fb543f80f56a403bd1
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2018-04-11 22:41:24 -0600

    Changed plottable returns to be an array

--- fcq.fun/src/app/httprequestor.ts
+++ fcq.fun/src/app/httprequestor.ts
@@ -1,5 +1,6 @@
 import {DataStore} from "./data-store"
 import {Plottable} from "./plottable"
+import {Lecturer} from "./lecturer"
 import { HttpClient, HttpHeaders  } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import * as XLSX from 'xlsx';
@@ -9,24 +10,24 @@ type AOA = any[][];
 export class HTTPRequestor implements DataStore {
   constructor(private http: HttpClient) {  }
 
-  public getPlottable(name: string): Plottable {
+  public getPlottable(name: string): Plottable[] {
     // Regex match - Is term a professor or class code?
 
 
     // Pull up the FCQ site and print out the first form
-    this.http.get('https://cors-anywhere.herokuapp.com/https://a2y9euj5ml.execute-api.us-east-2.amazonaws.com/prod/fcq-fetcher?instructor=' + name).subscribe(data => this.getXLS(data as string))
-    return null;
+    return this.http.get('https://cors-anywhere.herokuapp.com/https://a2y9euj5ml.execute-api.us-east-2.amazonaws.com/prod/fcq-fetcher?instructor=' + name).map(data => this.getXLS(data as string))
+
   }
 
-  private getXLS(url: string): void {
-    this.http.get('https://cors-anywhere.herokuapp.com/' + url, {responseType: 'arraybuffer'})
-        .subscribe(
-            data => this.parsePlottable(data),
+  private getXLS(url: string): Observable<Plottable[]> {
+    return this.http.get('https://cors-anywhere.herokuapp.com/' + url, {responseType: 'arraybuffer'})
+        .map(
+            data => { return this.parsePlottable(data) },
             error => console.log(error) // implement your error handling here
           )
   }
 
-  private parsePlottable(raw): void {
+  private parsePlottable(raw): Plottable[] {
     var binary = "";
     var bytes = new Uint8Array(raw);
     var length = bytes.byteLength;
@@ -41,13 +42,19 @@ export class HTTPRequestor implements DataStore {
 
     // Get all the data, going backward. This is one bit where we care whether it's a course or instructor
     const raw_data = <AOA>(XLSX.utils.sheet_to_json(ws, {raw: true}));
+
+    // Make a map of professor names to plottables
+    const plottables: Map<string, Plottable[]> = new Map();
+
     // Interate over the entries, if field matches something we want then add it
     for(var line in raw_data){
-      for(var key in raw_data[line]){
-        //console.log(key + " : " + raw_data[line][key])
-      }
+      // Make a lecturer of this line
+      plottables[line['Instructor']].append(line)
+
       console.log(raw_data[line])
     }
+
+    return null;
 
   }
 }
