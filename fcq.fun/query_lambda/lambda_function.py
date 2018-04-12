cb75e748ef53bb231f519f63e6da388d9af0c26f
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2018-04-12 11:18:49 -0600

    Update lambda_function.py

--- fcq.fun/query_lambda/lambda_function.py
+++ fcq.fun/query_lambda/lambda_function.py
@@ -49,6 +49,62 @@ def getFCQ(query):
     response = br.open(url)
 
     control = br.select_form("frmFCQ")
+    
+    #go through all of the form options so we can change them
+    for control in br.form.controls:
+        #this will show us all of our default value for the fields. See page options for the output of the FCQ page as of 1/3/15
+        #print "DEFAULT: type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
 
-    return control
+        if (control.name == 'fcqdpt'):
+            br[control.name] = [fcqdpt]
+            print "CHANGE fcqdpt type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+
+        elif (control.name == 'ftrm'):
+            br[control.name] = ['7'] # Fall
+            print "CHANGE first term type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+
+        elif (control.name == 'ltrm'):
+            br[control.name] = ['1'] # Spring
+            print "CHANGE last term type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+
+        elif (control.name == 'fileFrmt'):
+            br[control.name] = ['XLS']
+            print "CHANGE fileFrmt type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+
+        elif (control.name == 'fyr'):
+            br[control.name] = ['2007']
+            print "CHANGE first year type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+
+        elif (control.name == 'lyr'):
+            br[control.name] = ['2016']
+            print "CHANGE last year type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+            
+        elif (control.name == 'iname' and 'instructor' in query.keys()):
+            br[control.name] = query['instructor']
+            print "CHANGE instructor type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+            
+        elif (control.name == 'crse' and 'courseCode' in query.keys()):
+            br[control.name] = query['courseCode']
+            print "CHANGE courseCode type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+            
+        elif (control.name == 'subj' and 'courseDept' in query.keys()):
+            br[control.name] = query['courseDept']
+            print "CHANGE courseDept type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])    
+
+    response = br.submit()
+
+    if 'Currently, Excel files with FCQ results are limited to 50,000 rows.' in response.read():
+        print('CAUTION REQUEST HAS MORE THAN 50,000 LINES!')
+
+    for link in br.links():
+        if (link.text == 'Click here'):
+            print(link.url)
+            original = link.url
+            #need to split because output link is: javascript:popup('/fcqtemp/BD010395426.xls','BD010395426','750','550','yes','yes')
+            split = original.replace("\'","").split(',')
+            #get this BD010395426
+            proper_link = "https://fcq.colorado.edu/fcqtemp/{xcel}.xls".format(xcel=split[1])
+            print proper_link
+    
+    return proper_link
     
