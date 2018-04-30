6c12df28014944f9f3e0b1c2b98e196e12e6afa1
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2018-04-30 09:19:07 -0600

    Added required files.

---
+++ fcq.fun/src/app/chart.directive.ts
@@ -0,0 +1,10 @@
+import { Directive, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[chart-host]'
+})
+export class ChartDirective {
+
+  constructor(public viewContainerRef: ViewContainerRef) { }
+
+}
